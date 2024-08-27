a44f6084677693b41806cadb0b213238e0882406
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 18:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  	const obj = {
+  	const romanPairs= {
       0:['M',1000], 
       1:['D', 500], 
       2:['C', 100], 
@@ -25,12 +25,5 @@ function convertToRoman(num) {
     return result;
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
 module.exports = convertToRoman
